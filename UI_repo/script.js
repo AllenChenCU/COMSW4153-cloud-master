4a0652a8f3bcfcd75b4e1e117ec3b00d7073d3ab
nuclear-pasta02 <malini2700@gmail.com>
2024-09-29 09:33:46 -0400

    basic UI with start & end location
    basic UI with start & end location (mockup of UI with google maps api microservice)

---
+++ UI_repo/script.js
@@ -0,0 +1,27 @@
+document.getElementById('find_a_path').addEventListener('submit', function(e){
+    e.preventDefault();
+    const start = document.getElementById('start').value;
+    const end = document.getElementById('end').value;
+
+    document.getElementById('error-message').style.display = 'none';
+    
+    fetch('/find-route', {
+        method: 'POST',
+        headers:{
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({start, end})
+    })
+        .then(response => response.json())
+        .then(data => {
+            if(data.error){
+                document.getElementById('error-message').style.display = 'block';
+            }
+            else{
+                alert('Success! Accessible route found!');
+            }
+        })
+        .catch(error => {
+            document.getElementById('error-message').style.display = 'block';
+        });
+});
