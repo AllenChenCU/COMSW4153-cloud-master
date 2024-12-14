f59740c517558e126499d2e19b25fd15e7bc0481
amaribyrd <ab5311@columbia.edu>
2024-12-13 23:13:07 -0500

    add JWt authethication and fixed header error

--- api_gateway/server.js
+++ api_gateway/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const axios = require("axios");
 const cors = require('cors');
+const jwtSecret = process.env.JWT_SECRET;
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = 3000;
@@ -10,24 +12,33 @@ const port = 3000;
 const COMPOSITE_SERVICE_URL = "https://comsw4153-cloud-composite-service-973496949602.us-central1.run.app";
 app.use(express.json());
 app.use(cors({
-  origin: "*", // CHANFGE TO THE FINAL URL
+  origin: "*", 
   credentials: true
 }));
 
 app.use(session({
   secret: 'secret',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false,
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
-function isAuthenticated(req, res, next){
-  if(req.isAuthenticated()){
-    return next();
+
+function validateAuthHeader(req, res, next){
+  const token = req.headers['authorization']?.replace('Bearer ', '');
+  if (!token) {
+    return res.status(401).json({ error: 'Authorization token is missing or invalid.' });
   }
-  res.status(401).json({
-    error: "User is not authenticated"
+
+  jwt.verify(token, jwtSecret, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ error: 'Invalid or expired token.' });
+    }
+    req.user = decoded; 
+    req.token = token;
+    next();
   });
 }
 
@@ -35,12 +46,15 @@ function isAuthenticated(req, res, next){
 //curl "http://localhost:3000/query-routes-and-stations?source=Columbia%20University&destination=John%20F.%20Kennedy%20International%20Airport&user_id=123"
 //output:
 //{"routes":[{"bounds":{"northeast":{"lat":40.808134,"lng":-73.7893588},"southwest"...
-app.get("/query-routes-and-stations", async(req, res) =>{
+app.get("/query-routes-and-stations", validateAuthHeader, async(req, res) =>{
   try{
     const { source, destination, user_id } = req.query;
-    console.log("Querying routes and stations for source: ", source, " and destination: ", destination);
+
     const response = await axios.get(`${COMPOSITE_SERVICE_URL}/query-routes-and-stations/`,{
-      params: {source, destination, user_id}
+      params: {source, destination, user_id},
+      headers:{
+        'Authentication': `Bearer ${req.token}`
+      }
     });
     res.status(response.status).json(response.data);
   } catch(error){
@@ -53,10 +67,13 @@ app.get("/query-routes-and-stations", async(req, res) =>{
 //curl -X POST "http://localhost:3000/save-route" -H "Content-Type: application/json" -d @example_route.json
 //output:
 //{"message":"Route is successfully saved!","route_id":"b07e4b46-5b71-45d2-b123-d0ac194a92ed","user_id":"123"....
-app.post("/save-route", async(req, res) => {
+app.post("/save-route", validateAuthHeader, async(req, res) => {
   try{
     const response = await axios.post(`${COMPOSITE_SERVICE_URL}/save-route/`, req.body,{
-      headers: {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Headers": "Content-Type"},
+      headers: {
+        "Content-Type": "application/json", 
+        'Authentication': `Bearer ${req.token}`
+      }
     });
     res.status(response.status).json(response.data);
   } catch(error){
@@ -69,12 +86,15 @@ app.post("/save-route", async(req, res) => {
 //curl -X PUT "http://localhost:3000/unsave-route?route_id=a38349e6-b04d-40da-8540-53c6d495bb9e"
 //output:
 //{"message":"Route is successfully deleted!","route_id":"a38349e6-b04d-40da-8540-53c6d495bb9e"}
-app.put('/unsave-route', async(req, res) => {
+app.put('/unsave-route', validateAuthHeader, async(req, res) => {
   const { route_id } = req.query;
   try{
     console.log("Unsaving route with ID: ", route_id);
     const response = await axios.put(`${COMPOSITE_SERVICE_URL}/unsave-route/`, null,{
       params: {route_id},
+      headers:{
+        'Authentication': `Bearer ${req.token}`
+      }
     });
     res.status(response.status).json(response.data);
   } catch(error){
@@ -91,7 +111,10 @@ app.get("/get-saved-routes-and-stations", async (req, res) => {
   try{
     const { user_id } = req.query;
     const response = await axios.get(`${COMPOSITE_SERVICE_URL}/get-saved-routes-and-stations/`, {
-      params: {user_id}
+      params: {user_id},
+      headers:{
+        'Authentication': `Bearer ${req.token}`
+      }
     });
     res.status(response.status).json(response.data);
   } catch(error){
@@ -104,11 +127,14 @@ app.get("/get-saved-routes-and-stations", async (req, res) => {
 //curl "http://localhost:3000/query-all-routes-by-user?user_id=123&limit=10&page=1"
 //output:
 //{"routes":[{"id":13,"origin":"Columbia University","destination":"John F. Kennedy International Airport"...
-app.get("/query-all-routes-by-user", async (req, res) => {
+app.get("/query-all-routes-by-user", validateAuthHeader, async (req, res) => {
   try {
     const {user_id, limit, page} = req.query;
     const response = await axios.get(`${COMPOSITE_SERVICE_URL}/query-all-routes-by-user/`,{
-      params: {user_id, limit, page}
+      params: {user_id, limit, page},
+      headers:{
+        'Authentication': `Bearer ${req.token}`
+      }
     });
     res.status(response.status).json(response.data);
   } catch(error){
